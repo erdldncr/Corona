7b8cb0a1f4215611f47e308fbf00ba1231ce6ecb
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-03-22 19:35:27 +0000

    done

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-fetch('https://covid-api.mmediagroup.fr/v1/cases?country=Turkey').then(response => {
-  if (response.ok) {
-    return response.json();
-  }
-  throw new Error('Request failed!');
-}, networkError => {
-  console.log(networkError.message);
-}).then(jsonResponse => {
-  return console.log(jsonResponse)
-});
+const countryInputt=document.getElementById('inputCountry')
+const searchBtn=document.getElementById('search')
+const countryNAme=document.getElementById('countryName')
+const totalCase=document.getElementById('totalCase')
+const totalDeath=document.getElementById('totalDeaths')
+
+
+searchBtn.addEventListener('click',async()=>{
+  let countryName=(countryInputt.value)
+  let response= await fetch('https://covid-api.mmediagroup.fr/v1/cases?country='+countryName)
+        response= await response.json()
+        response=response.All
+        console.log(response)
+       countryName.innerHTML=response.country
+       totalCase.innerHTML=response.confirmed
+       totalDeath.innerHTML=response.deaths
+})
